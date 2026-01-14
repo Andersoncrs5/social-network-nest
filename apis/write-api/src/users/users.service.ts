7802c51ed1235a2adcb286e35b703365f55d430d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-14 20:55:23 -0300

    New method findOneById

--- apis/write-api/src/users/users.service.ts
+++ apis/write-api/src/users/users.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRepository } from './users.repository';
+import { ModelNotFoundException } from '@app/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+constructor(
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async findOneById(id: string): Promise<User> {
+    const user: User | null = await this.userRepository.findById(id)
+
+    if (user == null) {
+      throw new ModelNotFoundException("User not found")
+    }
+
+    return user;
+  }
+  
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,15 +29,12 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
+  
+  update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} user`;
   }
 }
