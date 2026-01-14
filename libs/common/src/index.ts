c17000d547189a6f591901c8cd70b0d1d3d8f338
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-14 20:29:12 -0300

    new classes to exports

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,3 +1,6 @@
 export * from './decorators/is-id.decorator';
 export * from './repositories/base.repository';
 export * from './base/base.entity';
+export * from './exceptions/modelNotFound.exception'
+export * from './res/http/response.res'
+export * from './interceptors/response.interceptor'
