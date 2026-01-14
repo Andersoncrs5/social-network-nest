8c06e0c1161939c182f0dee43f409b0f53891909
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-14 10:26:52 -0300

    Impled and configured User entity

--- apis/write-api/src/users/entities/user.entity.ts
+++ apis/write-api/src/users/entities/user.entity.ts
@@ -1,21 +1,27 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity } from "src/utils/bases/base.entity";
+import { Column, Entity, Index } from "typeorm";
 
-@Entity()
-export class User {
+@Entity('users')
+@Index('idx_username', ['username'])
+@Index('idx_email', ['email'])
+export class User extends BaseEntity {
+  
+  @Column({ length: 100, nullable: false })
+  name: string;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column({ length: 100, unique: true, nullable: false })
+  username: string;
 
-    @Column({ type: "varchar", length: 250, nullable: false })
-    name: string;
+  @Column({ length: 150, unique: true, nullable: false })
+  email: string;
 
-    @Column({ type: "varchar", unique: true, length: 250, nullable: false })
-    email: string;
+  @Column({ length: 300, nullable: false, select: false }) 
+  password: string;
 
-    @Column({ type: "varchar", length: 250, nullable: false })
-    password: string;
+  @Column({ length: 500, nullable: true })
+  refreshToken: string | null;
 
-    @Column({ type: "varchar" , length: 500, nullable: true})
-    refreshToken: string | null;
+  @Column({ type: 'timestamp', nullable: true })
+  loginBlockAt: Date | null;
 
 }
