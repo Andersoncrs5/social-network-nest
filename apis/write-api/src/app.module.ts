40c7b7ba323838228662b1d60de39b331bf5234e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-14 20:54:35 -0300

    Configured access the TiDB

--- apis/write-api/src/app.module.ts
+++ apis/write-api/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', 
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 4000,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || 'social_network_nest',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
