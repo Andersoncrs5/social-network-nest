f9bcb8865864abd3ffe46d6e1180c8db66c88b21
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-22 10:37:42 -0300

    Added UniqueEmailValidator and UniqueEmailValidator in providers

--- apis/write-api/src/users/users.module.ts
+++ apis/write-api/src/users/users.module.ts
@@ -4,15 +4,16 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserRepository } from './users.repository';
+import { UniqueEmailValidator } from './decorators/user/unique-email-user.decorator';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]) 
-  ],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [
-    UsersService, 
-    UserRepository
+    UsersService,
+    UserRepository,
+    UniqueEmailValidator,
+    UniqueEmailValidator,
   ],
   exports: [UsersService],
 })
