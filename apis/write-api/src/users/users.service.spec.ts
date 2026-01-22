22c9a485f761fa45a484313064aa03322ab382c8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-22 09:48:48 -0300

    More tests

--- apis/write-api/src/users/users.service.spec.ts
+++ apis/write-api/src/users/users.service.spec.ts
@@ -7,18 +7,25 @@ import { User } from './entities/user.entity';
 describe('UsersService', () => {
   let service: UsersService;
   let repository: UserRepository;
+  const mockUser = {
+    id: '1',
+    name: 'Darkness',
+    email: 'darkness@gmail.com',
+  } as User;
 
   const mockUserRepository = {
     findById: jest.fn(),
+    delete: jest.fn(),
   };
 
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         {
           provide: UserRepository,
-          useValue: mockUserRepository, 
+          useValue: mockUserRepository,
         },
       ],
     }).compile();
@@ -32,22 +39,71 @@ describe('UsersService', () => {
   });
 
   describe('findOneById', () => {
-    it('should return a user if found', async () => {
-      const mockUser = { id: '1', name: 'Darkness' } as User;
+    it('should return user when find by id', async () => {
       mockUserRepository.findById.mockResolvedValue(mockUser);
 
-      const result = await service.findOneById('1');
+      const user: User | null = await service.findOneById(mockUser.id);
+
+      expect(user).not.toBeNull();
+      expect(user!.id).toBe(mockUser.id);
+      expect(mockUserRepository.findById).toHaveBeenCalledWith('1');
+      expect(mockUserRepository.findById).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return null when findById', async () => {
+      mockUserRepository.findById.mockResolvedValue(null);
+
+      const user = await service.findOneById(mockUser.id);
+
+      expect(user).toBeNull();
+      expect(mockUserRepository.findById).toHaveBeenCalledWith('1');
+      expect(mockUserRepository.findById).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findOneByIdSimple', () => {
+    it('should return a user if found', async () => {
+      mockUserRepository.findById.mockResolvedValue(mockUser);
+
+      const result = await service.findOneByIdSimple('1');
 
       expect(result).toEqual(mockUser);
-      expect(repository.findById).toHaveBeenCalledWith('1');
+      expect(mockUserRepository.findById).toHaveBeenCalledWith('1');
+      expect(mockUserRepository.findById).toHaveBeenCalledTimes(1);
     });
 
     it('should throw ModelNotFoundException if user not found', async () => {
       mockUserRepository.findById.mockResolvedValue(null);
 
-      await expect(service.findOneById('999')).rejects.toThrow(
+      await expect(service.findOneByIdSimple('999')).rejects.toThrow(
         ModelNotFoundException,
       );
+      expect(mockUserRepository.findById).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete user by id', async () => {
+      mockUserRepository.findById.mockResolvedValue(mockUser);
+      mockUserRepository.delete.mockResolvedValue(undefined);
+
+      await service.remove(mockUser.id);
+
+      expect(mockUserRepository.findById).toHaveBeenCalledWith(mockUser.id);
+      expect(mockUserRepository.delete).toHaveBeenCalledWith(mockUser.id);
+
+      expect(mockUserRepository.findById).toHaveBeenCalledTimes(1);
+      expect(mockUserRepository.delete).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw ModelNotFoundException', async () => {
+      mockUserRepository.findById.mockResolvedValue(null);
+
+      await expect(service.remove('999')).rejects.toThrow(
+        ModelNotFoundException,
+      );
+
+      expect(mockUserRepository.delete).not.toHaveBeenCalled();
     });
   });
 });
