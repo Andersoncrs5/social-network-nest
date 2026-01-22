209ae5accb8f87f156cea6fbc70e52b95d03df64
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-22 13:00:39 -0300

    Added UserMapper and CryptoService in providers section

--- apis/write-api/src/users/users.module.ts
+++ apis/write-api/src/users/users.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { UserRepository } from './users.repository';
-import { UniqueEmailValidator } from './decorators/user/unique-email-user.decorator';
+import { Module } from '@nestjs/common';
+import { User } from './entities/user.entity.js';
+import { UsersController } from './users.controller.js';
+import { UsersService } from './users.service.js';
+import { UserRepository } from './users.repository.js';
+import { UniqueEmailValidator } from './decorators/user/unique-email-user.decorator.js';
+import { CryptoService } from '../configs/security/crypto/crypto.service.js';
+import { UserMapper } from '../utils/mappers/user-profile.mapper.js';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -14,6 +16,8 @@ import { UniqueEmailValidator } from './decorators/user/unique-email-user.decora
     UserRepository,
     UniqueEmailValidator,
     UniqueEmailValidator,
+    UserMapper,
+    CryptoService,
   ],
   exports: [UsersService],
 })
