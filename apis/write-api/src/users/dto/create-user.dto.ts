6884b31fdd416cfed33b71db580944e91125369c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-22 10:35:38 -0300

    Added validations

--- apis/write-api/src/users/dto/create-user.dto.ts
+++ apis/write-api/src/users/dto/create-user.dto.ts
@@ -1,6 +1,24 @@
+import { UniqueEmail } from '../decorators/user/unique-email-user.decorator';
+import { UniqueUsername } from '../decorators/user/unique-username.decorator';
+import { EmailConstraint } from '@app/common/decorators/email-constraint.decorator';
+import { MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @MinLength(5)
+  @MaxLength(100)
   name: string;
+
+  @MinLength(5)
+  @MaxLength(100)
+  @UniqueUsername()
   username: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @EmailConstraint()
+  @UniqueEmail()
   email: string;
+
+  @MinLength(8)
+  @MaxLength(50)
   password: string;
 }
